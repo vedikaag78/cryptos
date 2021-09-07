ef0a172d9a50b031d1d34b7ea33f4601c733d5e0
vedikaag78 <vedika1510@gmail.com>
2021-09-07 13:41:56 +0530

    Set 1 5

--- set 1/5.repeatxor.py
+++ set 1/5.repeatxor.py
@@ -1,20 +1,15 @@
 def en_xor(x , k ,y):
-   text = x
    encrpted = []
    for i in range(0, len(x)):
-       encrpted.append(text[i] ^ k[i % y])
+       encrpted.append(x[i] ^ k[i % y])
    return bytes(encrpted)
   
-
-
-
-
 def main():
-    s = input(b"The text:")
+    s = input("Enter the string: ").encode()
     key = b"ICE"
     key_lenth = len(key)
-    print("THe encrypted text:" , en_xor(s,key,key_lenth).hex())
+    print("THe encrypted text: " , en_xor(s,key,key_lenth).hex())
     
 
-if __name__ == '__main__ ':
+if __name__ == "__main__":
     main()
